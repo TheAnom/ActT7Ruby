667bf79b18fe5fd916f258b8cca6c85d03e25ce7
Jeferson Escobar <jefersonjosue255@gmail.com>
2025-09-08 18:42:13 -0600

    Generacion de Scaffold

---
+++ app/controllers/contactos_controller.rb
@@ -0,0 +1,70 @@
+class ContactosController < ApplicationController
+  before_action :set_contacto, only: %i[ show edit update destroy ]
+
+  # GET /contactos or /contactos.json
+  def index
+    @contactos = Contacto.all
+  end
+
+  # GET /contactos/1 or /contactos/1.json
+  def show
+  end
+
+  # GET /contactos/new
+  def new
+    @contacto = Contacto.new
+  end
+
+  # GET /contactos/1/edit
+  def edit
+  end
+
+  # POST /contactos or /contactos.json
+  def create
+    @contacto = Contacto.new(contacto_params)
+
+    respond_to do |format|
+      if @contacto.save
+        format.html { redirect_to @contacto, notice: "Contacto was successfully created." }
+        format.json { render :show, status: :created, location: @contacto }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @contacto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /contactos/1 or /contactos/1.json
+  def update
+    respond_to do |format|
+      if @contacto.update(contacto_params)
+        format.html { redirect_to @contacto, notice: "Contacto was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @contacto }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @contacto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /contactos/1 or /contactos/1.json
+  def destroy
+    @contacto.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to contactos_path, notice: "Contacto was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contacto
+      @contacto = Contacto.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def contacto_params
+      params.expect(contacto: [ :nombre, :email, :mensaje ])
+    end
+end
---
+++ app/models/contacto.rb
@@ -0,0 +1,7 @@
+class Contacto < ApplicationRecord
+
+  validates :nombre, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates :mensaje, presence: true
+
+end
