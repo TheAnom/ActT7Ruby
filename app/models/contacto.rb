6c9bd80b23cc45b7a72dd09ee34159f0917bd15c
Jeferson Escobar <jefersonjosue255@gmail.com>
2025-09-08 18:46:08 -0600

    Validaciones al modelo

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -2,6 +2,6 @@ class Contacto < ApplicationRecord
 
   validates :nombre, presence: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
-  validates :mensaje, presence: true
+  validates :mensaje, presence: true 
 
 end
